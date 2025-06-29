eca16cec881f7e9d2317a5e77de3779e6dfa78ee
LahiruKasuniDilhara <lahirukasunidilhara@lahirudilhara.lahirudilhara.me>
2025-06-29 10:48:12 +0530

    created the project and initialized the base version of it

---
+++ src/main/java/me/lahirudilhara/schat/SChatApplication.java
@@ -0,0 +1,13 @@
+package me.lahirudilhara.schat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/lahirudilhara/schat/SChatApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lahirudilhara.schat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
